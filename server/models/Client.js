a40a1e0c331c7c156c5a254148fd47d1c4f6ac91
Stephen Helman <stephenhelman18@gmail.com>
2024-10-07 22:46:27 -0600

    add timestamps to Client

--- server/models/Client.js
+++ server/models/Client.js
@@ -1,33 +1,41 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const clientSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
+const clientSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+    stage: {
+      type: String,
+      required: true,
+    },
+    calendarLink: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Url",
+      required: false,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  fullName: {
-    type: String,
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: false,
-  },
-  stage: {
-    type: String,
-    required: true,
-  },
-  calendarLink: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Url",
-    required: false,
-  },
-});
+  {
+    timestamps: {
+      createdAt: false,
+      updatedAt: true,
+    },
+  }
+);
 
 export default mongoose.model("Client", clientSchema);
